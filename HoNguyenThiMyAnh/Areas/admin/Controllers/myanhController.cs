f830129a53ed0b2fe108eb782877424847a391df
Ho Nguyen Thi My Anh <mianh211022@gmail.com>
2023-03-16 13:13:25 +0700

    first commit

---
+++ HoNguyenThiMyAnh/Areas/admin/Controllers/myanhController.cs
@@ -0,0 +1,20 @@
+﻿using HoNguyenThiMyAnh.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoNguyenThiMyAnh.Areas.admin.Controllers
+{
+    public class myanhController : Controller
+    {
+        WEBBANHANGEntities dbObj = new WEBBANHANGEntities();
+        // GET: admin/myanh
+        public ActionResult Index()
+        {
+            var lstProduct = dbObj.Products.ToList();
+            return View(lstProduct);
+        }
+    }
+}
