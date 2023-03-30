3573c130c854ede81eb1bff909ff1e0049daad4d
Ho Nguyen Thi My Anh <mianh211022@gmail.com>
2023-03-30 09:40:12 +0700

    new3003

--- HoNguyenThiMyAnh/Areas/admin/Controllers/ProductController.cs
+++ HoNguyenThiMyAnh/Areas/admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using HoNguyenThiMyAnh.Context;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -18,10 +20,11 @@ namespace HoNguyenThiMyAnh.Areas.admin.Controllers
             var lstProduct = dbObj.Products.ToList();
             return View(lstProduct);
         }
+
         [HttpGet]
         public ActionResult Create()
         {
-            
+
             return View();
         }
         [HttpPost]
@@ -29,29 +32,66 @@ namespace HoNguyenThiMyAnh.Areas.admin.Controllers
         {
             try
             {
-                if(objProduct.ImageUpload != null)
+                if (objProduct.ImageUpload != null)
                 {
                     string fileName = Path.GetFileNameWithoutExtension(objProduct.ImageUpload.FileName);
                     string extension = Path.GetExtension(objProduct.ImageUpload.FileName);
-                    fileName = fileName  + "_" + long.Parse(DateTime.Now.ToString("yyyyMMddhhmmss")) + extension;
+                    fileName = fileName + "_" + long.Parse(DateTime.Now.ToString("yyyyMMddhhmmss")) + extension;
                     objProduct.Avatar = fileName;
-                    objProduct.ImageUpload.SaveAs(Path.Combine(Server.MapPath(" ~/Content/image/"), fileName));
+                    //lưu file hình
+                    objProduct.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Content/images/"), fileName));
                 }
                 dbObj.Products.Add(objProduct);
                 dbObj.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch(Exception)
+            catch (Exception)
             {
                 return RedirectToAction("Index");
             }
+
         }
+
         [HttpGet]
         public ActionResult Details(int id)
         {
             var objProduct = dbObj.Products.Where(n => n.Id == id).FirstOrDefault();
             return View(objProduct);
         }
-
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var objProduct = dbObj.Products.Where(n => n.Id == id).FirstOrDefault();
+            return View(objProduct);
+        }
+        [HttpPost]
+        public ActionResult Delete(Product objPro)
+        {
+            var objProduct = dbObj.Products.Where(n => n.Id == objPro.Id).FirstOrDefault();
+            dbObj.Products.Remove(objProduct);
+            dbObj.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var objProduct = dbObj.Products.Where(n => n.Id == id).FirstOrDefault();
+            return View(objProduct);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Product objProduct)
+        {
+            if (objProduct.ImageUpload != null)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(objProduct.ImageUpload.FileName);
+                string extension = Path.GetExtension(objProduct.ImageUpload.FileName);
+                fileName = fileName + "_" + long.Parse(DateTime.Now.ToString("yyyyMMddhhmmss")) + extension;
+                objProduct.Avatar = fileName;
+                objProduct.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Content/images/"), fileName));
+            }
+            dbObj.Entry(objProduct).State = EntityState.Modified;
+            dbObj.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
